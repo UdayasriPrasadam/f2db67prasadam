7b1e37598a15f9a3af9c9529512638297de3957d
UdayasriPrasadam <111814533+UdayasriPrasadam@users.noreply.github.com>
2022-11-15 19:54:53 -0600

    routes modified

--- routes/gadgets.js
+++ routes/gadgets.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', gadgets.gadgets_view_all_Page);
+router.get('/detail', gadgets.gadgets_view_one_Page);
 
 module.exports = router;
