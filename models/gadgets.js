efc3e8aae0c73e367feb35a0c31ae135c09f2759
UdayasriPrasadam <111814533+UdayasriPrasadam@users.noreply.github.com>
2022-11-28 23:14:52 -0600

    validator added

--- models/gadgets.js
+++ models/gadgets.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
 const gadgetsSchema = mongoose.Schema({
 gadget_type: String,
-gadget_price: Number,
-gadget_version: String
+gadget_version: String,
+gadget_price: {
+    type: Number,
+    min: [100, "Min value is 100"],
+    max: [10000, "Max value is 5000"]
+}
 })
-module.exports = mongoose.model("gadgets",
-gadgetsSchema)
+module.exports = mongoose.model("gadgets", gadgetsSchema)
