ac82a2693a02b49fe81787da34b1d3b9b4ac7f03
UdayasriPrasadam <111814533+UdayasriPrasadam@users.noreply.github.com>
2022-11-30 16:52:29 -0600

    files modified

--- models/gadgets.js
+++ models/gadgets.js
@@ -8,7 +8,7 @@ gadget_version: String,
 gadget_price: {
     type: Number,
     min: [100, "Min value is 100"],
-    max: [6000, "Max value is 6000"]
+    max: [8000, "Max value is 8000"]
 }
 })
 module.exports = mongoose.model("gadgets", gadgetsSchema)
